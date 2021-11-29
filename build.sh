2fba0c6b74d8815c18bb59fca4aac3da3ac80dc0
Dave Townsend <dtownsend@oxymoronical.com>
2021-11-29 14:51:38 +0000

    Pull before running the build.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
 curl -L "https://dev.mysql.com/get/Downloads/MySQL-8.0.20/mysql-boost-8.0.20.tar.gz" > mysql.tar.gz
+docker pull alpine:3.12
 time docker run --rm -it -v ${PWD}:/src alpine:3.12 /src/build-script
