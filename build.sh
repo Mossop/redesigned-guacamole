215354145092e826b3144fe0d578feaf54b16c5d
Dave Townsend <dtownsend@oxymoronical.com>
2021-11-29 14:48:41 +0000

    Switch to mysql build.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-time docker run --rm -it -v ${PWD}:/src ubuntu:18.04 /src/build-script
+curl -L "https://dev.mysql.com/get/Downloads/MySQL-8.0.20/mysql-boost-8.0.20.tar.gz" > mysql.tar.gz
+time docker run --rm -it -v ${PWD}:/src alpine:3.12 /src/build-script
